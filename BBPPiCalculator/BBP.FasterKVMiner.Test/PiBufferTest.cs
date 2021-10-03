74b085468a3f5f619bee943929ff44a05423b4e8
Jessica Mulein <jessica@mulein.com>
2021-10-03 16:21:27 -0700

    start stubbing out tests

---
+++ BBPPiCalculator/BBP.FasterKVMiner.Test/PiBufferTest.cs
@@ -0,0 +1,13 @@
+namespace BBP.FasterKVMiner.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class PiBufferTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ BBPPiCalculator/BBP.FasterKVMiner.Test/TrackerTest.cs
@@ -0,0 +1,13 @@
+namespace BBP.FasterKVMiner.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TrackerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
