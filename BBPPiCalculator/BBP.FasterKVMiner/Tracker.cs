25c54074400dc5babf6f17aca51a01a7623630c6
Jessica Mulein <jessica@mulein.com>
2021-10-02 17:26:44 -0700

    stubs

---
+++ BBPPiCalculator/BBP.FasterKVMiner/Tracker.cs
@@ -0,0 +1,37 @@
+﻿namespace BBP.FasterKVMiner
+{
+    public class Tracker : IDisposable
+    {
+        private FasterKVBBPiMiner fasterKV;
+
+        /// <summary>
+        /// Example
+        /// </summary>
+        private readonly int[] BlockSizes = new int[]
+        {
+            10,
+            100,
+            1000,
+            10000
+        };
+
+        public Tracker()
+        {
+            this.fasterKV = new FasterKVBBPiMiner();
+        }
+
+        public async Task<WorkBlock> StartWork(long startingOffset)
+        {
+            PiDigit pd = new PiDigit(startingOffset);
+            var workBlock = new WorkBlock(
+                startingOffset: startingOffset,
+                blockSizes: this.BlockSizes);
+            return workBlock;
+        }
+
+        public void Dispose()
+        {
+            this.fasterKV.Dispose();
+        }
+    }
+}
---
+++ BBPPiCalculator/BBP.FasterKVMiner/WorkBlock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBP.FasterKVMiner
+{
+    public class WorkBlock
+    {
+        private readonly long StartingOffset;
+        private readonly int[] BlockSizes;
+
+        public WorkBlock(long startingOffset, int[] blockSizes)
+        {
+            this.StartingOffset = startingOffset;
+            this.BlockSizes = blockSizes;
+        }
+    }
+}
