cb47da466bfb62827c88c5ea48d1ed1461552b21
Jessica Mulein <jessica@mulein.com>
2021-10-04 09:48:00 -0700

    add overflow test

--- BBPPiCalculator/BBP.FasterKVMiner.Test/PiBufferTest.cs
+++ BBPPiCalculator/BBP.FasterKVMiner.Test/PiBufferTest.cs
@@ -1,5 +1,6 @@
 namespace BBP.FasterKVMiner.Test
 {
+    using System;
     using System.Linq;
     using BBP.FasterKVMiner;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -19,20 +20,28 @@ namespace BBP.FasterKVMiner.Test
                 maximum: 20);
             Assert.IsTrue(piBytes.SequenceEqual(new byte[] { 0xA3, 0x08, 0xD3, 0x13, 0x19 }));
 
-            var piBytesEarly = piBuffer.GetPiSegment(
+            var piBytesTestPrepend = piBuffer.GetPiSegment(
                 minimum: 0,
                 maximum: 20);
-            Assert.IsTrue(piBytesEarly.SequenceEqual(new byte[] { 0x24, 0x3F, 0x6A, 0x88, 0x85, 0xA3, 0x08, 0xD3, 0x13, 0x19 }));
+            Assert.IsTrue(piBytesTestPrepend.SequenceEqual(new byte[] { 0x24, 0x3F, 0x6A, 0x88, 0x85, 0xA3, 0x08, 0xD3, 0x13, 0x19 }));
 
-            var piBytesLate = piBuffer.GetPiSegment(
+            var piBytesTestAppend = piBuffer.GetPiSegment(
                 minimum: 0,
                 maximum: 30);
-            Assert.IsTrue(piBytesLate.SequenceEqual(new byte[] { 0x24, 0x3F, 0x6A, 0x88, 0x85, 0xA3, 0x08, 0xD3, 0x13, 0x19, 0x8A, 0x2E, 0x03, 0x70, 0x73 }));
+            Assert.IsTrue(piBytesTestAppend.SequenceEqual(new byte[] { 0x24, 0x3F, 0x6A, 0x88, 0x85, 0xA3, 0x08, 0xD3, 0x13, 0x19, 0x8A, 0x2E, 0x03, 0x70, 0x73 }));
 
-            var piBytesPastBuffer = piBuffer.GetPiSegment(
+            // TODO: it's possible we might be able to screw things up by moving to an odd minimum- idiot proof?
+            var piBytesTestAppendTwice = piBuffer.GetPiSegment(
                 minimum: 0,
                 maximum: 40);
-            Assert.IsTrue(piBytesPastBuffer.SequenceEqual(new byte[] { 0x24, 0x3F, 0x6A, 0x88, 0x85, 0xA3, 0x08, 0xD3, 0x13, 0x19, 0x8A, 0x2E, 0x03, 0x70, 0x73, 0x44, 0xA4, 0x09, 0x38, 0x22 }));
+            Assert.IsTrue(piBytesTestAppendTwice.SequenceEqual(new byte[] { 0x24, 0x3F, 0x6A, 0x88, 0x85, 0xA3, 0x08, 0xD3, 0x13, 0x19, 0x8A, 0x2E, 0x03, 0x70, 0x73, 0x44, 0xA4, 0x09, 0x38, 0x22 }));
+
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                var piBytesTestTooBig = piBuffer.GetPiSegment(
+                minimum: 0,
+                maximum: 42);
+            });
         }
     }
 }
